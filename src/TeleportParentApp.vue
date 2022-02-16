70524fd3b0caedca5f5c638e414798fbf3a1b412
yodev <yodev2177@gmail.com>
2022-02-17 00:52:58 +0900

    Teleport機能

---
+++ src/TeleportParentApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+  <TeleportParent />
+</template>
+ 
+<script>
+import TeleportParent from "./components/TeleportParent";
+ 
+export default {
+  name: 'App',
+  components: {
+    TeleportParent
+  }
+}
+</script>
+ 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
