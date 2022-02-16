95c3a92d453fcbe6b80a913f670c01d22ac6109e
yodev <yodev2177@gmail.com>
2022-02-17 02:10:38 +0900

    SFC実装

---
+++ src/SfcApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <sfc-component message='青文字'></sfc-component>
+    <sfc-component message='赤文字' color='red'></sfc-component>
+    <sfc-component message='大きい赤文字' color='red' font_size='5rem'></sfc-component>
+  </div>
+</template>
+
+<script>
+  import SfcComponent from './components/SfcComponent.vue'
+  export default {
+    name: 'Sfc',
+    components: {
+      SfcComponent
+    }
+  }
+</script>
---
+++ src/components/SfcComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text">{{ message }}</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      color: {
+        type: String,
+        default: 'blue'
+      },
+      font_size: {
+        type: String,
+        default: '2rem'
+      },
+      message: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .text {
+    color: v-bind(color);
+
+    font-size: v-bind('font_size');
+  }
+</style>
